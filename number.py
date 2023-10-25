1a56a0974eafbce8a9df2879104c5bb3716c34a9
Lorenzo <lorenzonicotera02@gmail.com>
2023-10-25 22:23:07 +0200

    Addition of number logic and user's input logic

---
+++ number.py
@@ -0,0 +1,34 @@
+from math import * #questo si chiama modulo in quesot caso abbiamo
+#espanso il numero di math functions che possiamo utilizzare
+#tutorial che lavora con i numeri
+#variabile che contiene un numero
+n = 5
+neg_n = -5
+#stampa di un numero
+print(7)
+#alcuni calcoli matematici
+print(7 * 7)
+print(8.3433 * 2)
+#Calcoli in python: * moltiplicazione, + addizione
+#/ divisione % modulo (principalmente stampa il resto di una ipotetica divisione)
+print(n)
+#con una determinata funzione è possibile convertire
+#un numero in una stringa
+str_num = str(n)
+print(str_num)
+#funzioni di matematica avanzata
+#conversione in valore assoluto
+print(abs(neg_n))
+#potenza di un numero prende due valori
+#il numero e l'elevamento a potenza
+print(pow(3, 4))
+#funzione che stampa quale numero è più grande tra due numeri
+print(max(4, 6))
+#la funzione opposta
+print(min(4, 6))
+#funzione che arrotonda
+print(round(3.7))
+#in questa lezione importiamo pure guarda cosa mettiamo
+#all'inizio del file
+#la radice di un numero
+print(sqrt(36))
---
+++ user_input.py
@@ -0,0 +1,5 @@
+#utilizzeremo la funzione input per ricevere
+#l'input da tastiera dall'utente
+str = input("Enter your name: ")
+age = input("How old are you? ")
+print("Hi " + str + " you're " + age)
